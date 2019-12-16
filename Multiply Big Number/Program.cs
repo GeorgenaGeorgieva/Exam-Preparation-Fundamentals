3f9d288d2bdfa509a60121834513524b111f0728
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2019-12-16 21:50:06 +0200

    CSharp

---
+++ Multiply Big Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNumber = Console.ReadLine();
+            int multiplier = int.Parse(Console.ReadLine());
+            StringBuilder bigNumber = new StringBuilder();
+            int onMind = 0;
+
+            for (int i = inputNumber.Length - 1; i >= 0 ; i--)
+            {
+
+                int lastDigit = int.Parse(inputNumber[i].ToString());
+                int result = lastDigit * multiplier + onMind;
+                bigNumber.Append(result % 10);
+                onMind = result / 10;
+
+            }
+            if (onMind != 0)
+            {
+                bigNumber.Append(onMind);
+            }
+            string resultNumber = string.Join("", bigNumber.ToString().Reverse()).TrimStart('0');
+            if (resultNumber == String.Empty)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(resultNumber);
+            }
+            
+        }
+    }
+}
---
+++ Replace Repeating Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < inputLine.Length - 1; i++)
+            {
+                string currentLetter = inputLine[i].ToString();
+                string nextLetter = inputLine[i + 1].ToString();
+                if (currentLetter != nextLetter)
+                {
+                    builder.Append(currentLetter);
+                }
+            }
+            builder.Append(inputLine.Last());
+            Console.WriteLine(builder);
+        }
+    }
+}
