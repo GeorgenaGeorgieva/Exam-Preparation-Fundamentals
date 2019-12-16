0a773ec76aac113e8b416a89b8a1b969342989b8
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2019-12-16 21:57:59 +0200

    Update Program.cs

--- 2. Repeat Strings/Program.cs
+++ 2. Repeat Strings/Program.cs
@@ -10,6 +10,7 @@ namespace _2._Repeat_Strings
         {
             string[] words = Console.ReadLine().Split();
             StringBuilder result = new StringBuilder();
+            
             foreach (var word in words)
             {
                 for (int i = 0; i < word.Length; i++)
@@ -17,6 +18,7 @@ namespace _2._Repeat_Strings
                     result.Append(word);
                 }
             }
+            
             Console.WriteLine(result);
         }
     }
