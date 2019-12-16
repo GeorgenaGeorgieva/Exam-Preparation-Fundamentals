3fbc6e5dfda4190fce4101bfbec56c281e2871b2
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2019-12-16 21:56:02 +0200

    Update Program.cs

--- 1. Reverse Strings/Program.cs
+++ 1. Reverse Strings/Program.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
-
 namespace _1._Reverse_Strings
 {
     class Program
@@ -11,16 +10,20 @@ namespace _1._Reverse_Strings
         static void Main(string[] args)
         {
             string line = Console.ReadLine();
+            
             while (line != "end")
             {
                 string word = line;
                 string reversedWord = "";
+                
                 for (int i = word.Length - 1; i >=0; i--)
                 {
                     reversedWord += word[i];
                 }
+                
                 Console.Write($"{word} = {reversedWord}");
                 Console.WriteLine();
+                
                 line = Console.ReadLine();
             }
         }
