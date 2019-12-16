b4fae405ece4236ab2669558724dd72b0c21a2f0
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2019-12-16 21:51:19 +0200

    CSharp

---
+++ String Explosion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+
+namespace String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputLine = input.Split(">");
+            string result = inputLine[0];
+            int reminingPower = 0;
+            for (int i = 1; i < inputLine.Length; i++)
+            {
+                result += ">";
+                string currentString = inputLine[i];
+                char digitSymbol = currentString[0];
+                int power = int.Parse(digitSymbol.ToString()) + reminingPower;
+                if (power > currentString.Length)
+                {
+                    reminingPower = power - currentString.Length;
+                }
+                else
+                {
+                    result += currentString.Substring(power);
+                }     
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Valid Usernames/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userNames = Console.ReadLine().Split(", ");
+           
+            for (int i = 0; i < userNames.Length; i++)
+            {
+                string currentUser = userNames[i];
+                bool isLengthValid = true;
+                bool isContentValid = true;
+                if (currentUser.Length < 3 || currentUser.Length > 16)
+                {
+                    isLengthValid = false;
+                }
+                for (int k = 0; k < currentUser.Length; k++)
+                {
+                    char currentSymbol = currentUser[k];
+                    if (!char.IsLetterOrDigit(currentSymbol) && currentSymbol != '-' && currentSymbol != '_')
+                    {
+                        isContentValid = false;
+                        break;
+                    } 
+                }
+                if (isLengthValid && isContentValid)
+                {
+                    Console.WriteLine(currentUser);
+                }
+            }
+        }
+    }
+}
