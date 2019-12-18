9a83b996a46aa40f320cb2fcb60f7ed6ae9ba07f
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2019-12-18 12:27:53 +0200

    Update Program.cs

--- 4.Caesar Cipher/Program.cs
+++ 4.Caesar Cipher/Program.cs
@@ -8,15 +8,19 @@ namespace Caesar_Cipher
     {
         static void Main(string[] args)
         {
-            string inputLine = Console.ReadLine();          
+            string inputLine = Console.ReadLine(); 
+            
             StringBuilder encriptedText = new StringBuilder();
+            
             for (int i = 0; i < inputLine.Length; i++)
             { 
                     char currentSimbol = inputLine[i];
                     int numberNextSymbol = currentSimbol + 3;
                     char nextsymbol = (char)numberNextSymbol;
+                
                     encriptedText.Append(nextsymbol);    
             }
+            
             Console.WriteLine(encriptedText);
         }
     }
