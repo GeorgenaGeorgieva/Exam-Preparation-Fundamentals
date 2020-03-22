10c71101324e0f9a8f93d22eae1cb7cb9e7de67a
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-22 22:37:50 +0200

    C#

---
+++ 09. Regular Expressions/3. SoftUni Bar Income/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace _3._SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?)\$";
+            double totalMoneyForDay = 0;
+            string input = Console.ReadLine();
+            while (input != "end of shift")
+            {
+                MatchCollection matchProfitInformation = Regex.Matches(input, pattern);
+                double profitMoney = 0;    
+                foreach  (Match data in matchProfitInformation)
+                {
+                    string customerName = data.Groups["customer"].Value;
+                    string product = data.Groups["product"].Value;
+                    int count = int.Parse(data.Groups["count"].Value);
+                    double price = double.Parse(data.Groups["price"].Value);
+                    profitMoney = count * price;
+                    Console.WriteLine($"{customerName}: {product} - {profitMoney:F2}");
+                    totalMoneyForDay += profitMoney;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total income: {totalMoneyForDay:F2}");
+        }
+    }
+}
