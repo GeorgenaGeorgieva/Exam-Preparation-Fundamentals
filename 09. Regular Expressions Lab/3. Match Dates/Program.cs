351e5dc9b04a3d52d334f3663cb7d66c9dffe88a
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-22 22:35:34 +0200

    C#

---
+++ 09. Regular Expressions Lab/3. Match Dates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace _3._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b(?<day>\d{2})(?<sep>[-.\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b";
+            string dates = Console.ReadLine();
+            MatchCollection matchDates = Regex.Matches(dates, regex);
+            foreach  (Match date in matchDates)
+            {
+                string day = date.Groups["day"].Value;
+                string month = date.Groups["month"].Value;
+                string year = date.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
