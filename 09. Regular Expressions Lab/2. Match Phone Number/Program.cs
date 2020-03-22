c16251d990307426ad1cae14531d8862484e0fb9
Georgena Georgieva <57866154+GeorgenaGeorgieva@users.noreply.github.com>
2020-03-22 22:34:48 +0200

    C#

---
+++ 09. Regular Expressions Lab/2. Match Phone Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace _2._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"(\+359([ -])2(\2)(\d{3})(\2)(\d{4}))\b";
+            string phoneNumber = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(phoneNumber, regex);
+            var matchedPhones = matches
+                .Cast<Match>()
+                .Select(a => a.Value.Trim())
+                .ToArray();
+            Console.Write(string.Join(", ", matchedPhones));
+        }
+    }
+}
