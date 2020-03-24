fca907e559aef4e17c50f9c34f98e5dcb9697198
Georgena Georgieva <georgena1991@gmail.com>
2020-03-24 12:16:37 +0200

    Update Program.cs

--- 28. Strings and Text Processing/2. Repeat Strings/Program.cs
+++ 28. Strings and Text Processing/2. Repeat Strings/Program.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-
 namespace RepeatStrings
 {
+    using System;
+    using System.Linq;
+    using System.Text;
+    
     class Program
     {
         static void Main(string[] args)
         {
             string[] words = Console.ReadLine().Split();
+            
             StringBuilder result = new StringBuilder();
             
             foreach (var word in words)
